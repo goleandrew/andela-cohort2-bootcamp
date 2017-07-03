07392b62f71d590fb67adb721819535257bb0b30
Gole Andrew <iam@goleandrew.com>
2017-07-03 15:57:39 +0300

    file with test cases for the prime numbers function

--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -13,24 +13,24 @@ class TestPrimeNumbers(unittest.TestCase):
         self.assertEqual(prime_numbers(20), [2, 3, 5, 7, 11, 13, 17, 19])
 
     def test_zero(self):
-        """testing if zero is correctly determined not to be prime."""
+        """testing if 0 is correctly determined not to be a prime number."""
 
         self.assertEqual(prime_numbers(
-            0), "zero or one are not prime numbers.")
+            0), "0 or 1 are not prime numbers.")
 
     def test_one(self):
-        """"testing if one is correctly determined not to be prime."""
+        """"testing if one is correctly determined not to be prime number."""
 
         self.assertEqual(prime_numbers(
-            1), "zero or one are not prime numbers.")
+            1), "0 or 1 are not prime numbers.")
 
     def test_invalid_type_string_list(self):
-        """Testing if error returned for non-integer input."""
+        """testing if error returned for non-integer input."""
 
-        self.assertEqual(prime_numbers([]), "Only integers are allowed.")
+        self.assertEqual(prime_numbers([]), "only integers are allowed.")
 
     def test_only_positive(self):
-        """Testing if error returned for negative integers input."""
+        """testing if error returned for negative integers input."""
 
         self.assertEqual(
             prime_numbers(-1), "integers less than 2 cannot be prime numbers")
